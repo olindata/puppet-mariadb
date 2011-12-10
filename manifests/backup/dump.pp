84e6e0364df238d39d40fa5998e587294c944c67
Walter Heck <walter@tribily.com>
2011-12-10 11:27:31 +0200

    Replaced more stuff with mariadb params

--- manifests/backup/dump.pp
+++ manifests/backup/dump.pp
@@ -1,58 +1,58 @@
 class mariadb::backup::dump {
+  
   require mariadb::params
   
   include mariadb::backup::generic
   
   file {"${mariadb::params::backup_dir}/mysqldumps":
-    ensure   => "directory",
-    owner  => root,
+    ensure  => "directory",
+    owner   => root,
     group   => root,
-    require  => File["${::mysql-backup-dir}"]
+    require => File["${mariadb::params::backup_dir}"]
   }
 
   file {"/etc/mysql_backup.conf":
     ensure  => "present",
-    content   =>  template("mariadb/mysql_backup.conf.erb"),
-    owner  => root,
+    content => template("mariadb/mysql_backup.conf.erb"),
+    owner   => root,
     group   => root,
-    mode  => 500,
+    mode    => 500,
     require => [
-          File["${::mysql-backup-dir}/scripts"], 
-          File["${::mysql-backup-dir}/mysqldumps"], 
-          Package["mariadb-server"]
-          ]
+      File["${mariadb::params::backup_dir}"], 
+      File["${mariadb::params::backup_dir}/scripts"], 
+      File["${mariadb::params::backup_dir}/mysqldumps"], 
+      Package["mariadb-server"]
+    ]
   }
 
   file {"${mariadb::params::backup_dir}/scripts/mysql-dump-backup.sh":
     ensure  => "present",
-    content   =>  template("mariadb/mysql-dump-backup.sh.erb"),
-    owner  => root,
+    content => template("mariadb/mysql-dump-backup.sh.erb"),
+    owner   => root,
     group   => root,
-    mode  => 500,
+    mode    => 500,
     require => [
-          File["${mariadb::params::backup_dir}/scripts"], 
-          File["${mariadb::params::backup_dir}/mysqldumps"], 
-          Package["mariadb-client"]
-          ]
+      File["${mariadb::params::backup_dir}/scripts"], 
+      File["${mariadb::params::backup_dir}/mysqldumps"], 
+      Package["mariadb-client"]
+    ]
   }
 
-     file {"${mariadb::params::backup_dir}/scripts/mysql-dump-bkup-rotate.sh":
-                ensure  => "present",
-                content =>  template("mariadb/mysql-dump-bkup-rotate.sh.erb"),
-                owner   => root,
-                group   => root,
-                mode    => 500, 
-                require => [
-                                        File[${mariadb::params::backup_dir}]
-                                        ]       
-        }       
-
+  file {"${mariadb::params::backup_dir}/scripts/mysql-dump-bkup-rotate.sh":
+    ensure  => "present",
+    content => template("mariadb/mysql-dump-bkup-rotate.sh.erb"),
+    owner   => root,
+    group   => root,
+    mode    => 500, 
+    require => File[${mariadb::params::backup_dir}]
+  }       
 
   cron { "mysql-sqldump-backup":
     command => "$${mariadb::params::backup_dir}/scripts/mysql-dump-backup.sh",
-    user => root,
-    hour => 04,
-    minute  => 17
+    user    => root,
+    hour    => 04,
+    minute  => 17,
+    require => File["${mariadb::params::backup_dir}/scripts/mysql-dump-backup.sh"]
   }
   
 }
