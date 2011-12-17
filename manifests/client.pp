98c4b4f68efa731d93b413a4a53c8a03807abdc2
Walter Heck <walter@tribily.com>
2011-12-18 00:47:18 +0200

    Adding some comments

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,6 +1,9 @@
 class mariadb::client {
   
+  # We need the apt repo to be able to install the latest mariadb version
   require apt::repo::mariadb
+  
+  # require the params class so we have access to the params inside it
   require mariadb::params
     
   package { "mariadb-client":
