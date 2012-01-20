2f73fd09a891a3c88f0e49b49caa5f5fe7ef732c
Walter Heck <walter@tribily.com>
2012-01-20 10:11:20 +0200

    code layout fixes

--- manifests/backup/binary.pp
+++ manifests/backup/binary.pp
@@ -1,36 +1,38 @@
 class mariadb::backup::binary {
   
   require mariadb::params
+  
   include mariadb::backup::generic
+
   include xtrabackup
 
   file {"${mariadb::params::backup_dir}/scripts/mysql-binary-backup.sh":
     ensure  => "present",
-    content   =>  template("mariadb/mysql-binary-backup.sh.erb"),
-    owner  => root,
+    content => template("mariadb/mysql-binary-backup.sh.erb"),
+    owner   => root,
     group   => root,
-    mode  => 500,
+    mode    => 500,
     require => [
-          File["${mariadb::params::backup_dir}/scripts"], 
-          Package["mariadb-client"]
-          ]
+      File["${mariadb::params::backup_dir}/scripts"], 
+      Package["mariadb-client"]
+    ]
   }
 
   file {"${mariadb::params::backup_dir}/scripts/mysql-bin-bkup-rotate.sh":
     ensure  => "present",
-    content   =>  template("mariadb/mysql-bin-bkup-rotate.sh.erb"),
-    owner  => root,
+    content => template("mariadb/mysql-bin-bkup-rotate.sh.erb"),
+    owner   => root,
     group   => root,
-    mode  => 500,
+    mode    => 500,
     require => [
-          File["${mariadb::params::backup_dir}/scripts/mysql-binary-backup.sh"]
-          ]
+      File["${mariadb::params::backup_dir}/scripts/mysql-binary-backup.sh"]
+    ]
   }
 
   cron { "mysql-binary-backup":
     command => "${mariadb::params::backup_dir}/scripts/mysql-binary-backup.sh",
-    user => root,
-    hour => 02,
+    user    => root,
+    hour    => 02,
     minute  => 17
   }
 
