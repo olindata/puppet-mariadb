08e979a7257443ed09809cad012fe4d783c9d8bc
Walter Heck <walter@tribily.com>
2012-01-20 10:39:26 +0200

    small bugfix to specify path for mysql binary wehn creating a user

--- manifests/user.pp
+++ manifests/user.pp
@@ -7,17 +7,19 @@ define mariadb::user($username, $pw, $dbname, $grants = 'all privileges', $host_
   include mariadb::params
 
   if $withgrants {
-      exec { "create-grant-${name}-${username}-${dbhost}":
+    exec { "create-grant-${name}-${username}-${dbhost}":
       command => "/usr/bin/mysql -h${dbhost} -u${mariadb::params::admin_user} -p${mariadb::params::admin_pass} -e 'grant ${grants} on `${dbname}`.* to `${username}`@`${host_to_grant}` identified by \"${pw}\" with grant option'",
+      path    => "/bin:/usr/bin",
       onlyif  => "[ `/usr/bin/mysql -h${dbhost} -u${mariadb::params::admin_user} -p${mariadb::params::admin_pass} -BN -e 'select count(*) from mysql.db where User=\"${username}\" and Db=\"${dbname}\"'` -eq 0 ]",
       require => Package['mariadb-client']
     }
   } else {
-      exec { "create-grant-${name}-${username}-${dbhost}":
+    exec { "create-grant-${name}-${username}-${dbhost}":
       command => "/usr/bin/mysql -h${dbhost} -u${mariadb::params::admin_user} -p${mariadb::params::admin_pass} -e 'grant ${grants} on `${dbname}`.* to `${username}`@`${host_to_grant}` identified by \"${pw}\"'",
+      path    => "/bin:/usr/bin",
       onlyif  => "[ `/usr/bin/mysql -h${dbhost} -u${mariadb::params::admin_user} -p${mariadb::params::admin_pass} -BN -e 'select count(*) from mysql.db where User=\"${username}\" and Db=\"${dbname}\"'` -eq 0 ]",
       require => Package['mariadb-client']
     }
-    }
+  }
 }
 
