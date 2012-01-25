db5bb9d1e9ba25acc2c1c8e55c1ae2555f672812
Walter Heck <walter@tribily.com>
2012-01-25 08:01:46 +0200

    Add tribily mysql monitoring scripts

--- manifests/monitoring/tribily.pp
+++ manifests/monitoring/tribily.pp
@@ -3,6 +3,18 @@ class mariadb::monitoring::tribily {
   tribily::agent::userparams{ 'mariadb':
     file_src => 'puppet:///mariadb/monitoring/tribily.conf'
   }
+  
+  file { '/opt/tribily/bin/mysql_repl_status.pl':
+    ensure  => present,
+    content => template('mariadb/monitoring/mysql_repl_status.pl.erb'), 
+    
+  }
+
+  file { '/opt/tribily/bin/mysql_status.pl':
+    ensure  => present,
+    content => template('mariadb/monitoring/mysql_status.pl.erb'), 
+    
+  }
 
   mariadb::user{ 'tribilyagent':
     username      => $mariadb::params::monitor_user, 
