b6abb9fb33ccf2989d38bba342858031bba5b19f
Walter Heck <walter@tribily.com>
2012-01-25 05:04:25 +0200

    One more small fix

--- manifests/user.pp
+++ manifests/user.pp
@@ -14,14 +14,14 @@ define mariadb::user($username, $pw, $dbname, $grants = 'all privileges', $host_
 
   if $withgrants {
     exec { "create-grant-${name}-${username}-${dbhost}":
-      command => "/usr/bin/mysql -h${dbhost} -u${mariadb::params::admin_user} -p${mariadb::params::admin_pass} -e 'grant ${grants} on `${real_dbname}`.* to `${username}`@`${host_to_grant}` identified by \"${pw}\" with grant option'",
+      command => "/usr/bin/mysql -h${dbhost} -u${mariadb::params::admin_user} -p${mariadb::params::admin_pass} -e 'grant ${grants} on `${real_dbname}` to `${username}`@`${host_to_grant}` identified by \"${pw}\" with grant option'",
       path    => "/bin:/usr/bin",
       onlyif  => "[ `/usr/bin/mysql -h${dbhost} -u${mariadb::params::admin_user} -p${mariadb::params::admin_pass} -BN -e 'select count(*) from mysql.db where User=\"${username}\" and Db=\"${real_dbname}\"'` -eq 0 ]",
       require => Package['mariadb-client']
     }
   } else {
     exec { "create-grant-${name}-${username}-${dbhost}":
-      command => "/usr/bin/mysql -h${dbhost} -u${mariadb::params::admin_user} -p${mariadb::params::admin_pass} -e 'grant ${grants} on `${real_dbname}`.* to `${username}`@`${host_to_grant}` identified by \"${pw}\"'",
+      command => "/usr/bin/mysql -h${dbhost} -u${mariadb::params::admin_user} -p${mariadb::params::admin_pass} -e 'grant ${grants} on `${real_dbname}` to `${username}`@`${host_to_grant}` identified by \"${pw}\"'",
       path    => "/bin:/usr/bin",
       onlyif  => "[ `/usr/bin/mysql -h${dbhost} -u${mariadb::params::admin_user} -p${mariadb::params::admin_pass} -BN -e 'select count(*) from mysql.db where User=\"${username}\" and Db=\"${real_dbname}\"'` -eq 0 ]",
       require => Package['mariadb-client']
