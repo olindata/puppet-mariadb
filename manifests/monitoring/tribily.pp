8fb3f3865e24e7dcb1a60c39df65c50c97208dd4
Daniel Black <daniel.black@openquery.com>
2012-01-30 02:48:26 -0500

    add permissions to make scripts executable and hide passwords.

--- manifests/monitoring/tribily.pp
+++ manifests/monitoring/tribily.pp
@@ -1,17 +1,22 @@
 class mariadb::monitoring::tribily {
-  
+
   tribily::agent::userparams{ 'mariadb':
     content       => template('mariadb/monitoring/tribily.conf'),
   }
-  
+
   file { '/opt/tribily/bin/mysql_repl_status.pl':
     ensure  => present,
+    owner   => 'root',
+    group   => 'zabbix',
+    mode    => 510,
     content => template('mariadb/monitoring/mysql_repl_status.pl.erb'), 
-    
   }
 
   file { '/opt/tribily/bin/mysql_status.pl':
     ensure  => present,
+    owner   => 'root',
+    group   => 'zabbix',
+    mode    => 510,
     content => template('mariadb/monitoring/mysql_status.pl.erb'), 
     
   }
