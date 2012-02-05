fb689c4ac6396dbecd3b88794ebe8a5bf97e0b89
Walter Heck <walter@tribily.com>
2012-02-05 03:39:23 +0200

    proper checking of user privileges

--- manifests/user.pp
+++ manifests/user.pp
@@ -6,18 +6,6 @@ define mariadb::user ($username, $pw, $dbname, $grants = 'all privileges',
 
   include mariadb::params
 
-  if ($dbname == '*') {
-    $real_dbname = "*.*"
-  } else {
-    $real_dbname = "`${dbname}`.*"
-  }
-
-  if ($mariadb::params::admin_pass == '') {
-    $real_admin_pass = ''
-  } else {
-    $real_admin_pass = "-p\"${mariadb::params::admin_pass}\""
-  }
-
   if $withgrants {
     $grantoption = ' with grant option'
     $isgrantable = 'YES'
@@ -26,10 +14,24 @@ define mariadb::user ($username, $pw, $dbname, $grants = 'all privileges',
     $isgrantable = 'NO'
   }
 
+  if ($mariadb::params::admin_pass == '') {
+    $real_admin_pass = ''
+  } else {
+    $real_admin_pass = "-p\"${mariadb::params::admin_pass}\""
+  }
+
+  if ($dbname == '*') {
+    $real_dbname = "*.*"
+    $privilege_check_query = "select count(*) from information_schema.USER_PRIVILEGES where GRANTEE=\"'${username}'@'${dbhost}'\" and PRIVILEGE_TYPE=\"${grants}\" and IS_GRANTABLE=\"${isgrantable}\""
+  } else {
+    $real_dbname = "`${dbname}`.*"
+    $privilege_check_query = "select count(*) from information_schema.SCHEMA_PRIVILEGES where TABLE_SCHEMA=\"'${dbname}'\" GRANTEE=\"'${username}'@'${dbhost}'\" and PRIVILEGE_TYPE=\"${grants}\" and IS_GRANTABLE=\"${isgrantable}"
+  }
+
   exec { "create-grant-${name}-${username}-${dbhost}":
     command => "/usr/bin/mysql -h${dbhost} -u${mariadb::params::admin_user} ${real_admin_pass} -e 'grant ${grants} on ${real_dbname} to `${username}`@`${host_to_grant}` identified by \"${pw}\" ${grantoption}'",
     path    => "/bin:/usr/bin",
-    onlyif  => "[ `/usr/bin/mysql -h${dbhost} -u${mariadb::params::admin_user} ${real_admin_pass} -BN -e \"select count(*) from information_schema.USER_PRIVILEGES where GRANTEE=\\\"'${username}'@'${dbhost}'\\\" and PRIVILEGE_TYPE=\\\"${grants}\\\" and IS_GRANTABLE=\\\"${isgrantable}\\\"\"` -eq 0 ]",
+    onlyif  => "[ `/usr/bin/mysql -h${dbhost} -u${mariadb::params::admin_user} ${real_admin_pass} -BN -e \"${privilege_check_query}\"` -eq 0 ]",
     require => Package[$mariadb::params::packagename_client]
   }
 }
