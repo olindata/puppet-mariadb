7e697b5791386ac438be6e62d3c4647717df71b2
Daniel Black <daniel.black@openquery.com>
2012-02-05 13:15:41 +1000

    fix host_to_grant to its used where dbhost!=host_to_grant

--- manifests/user.pp
+++ manifests/user.pp
@@ -22,17 +22,16 @@ define mariadb::user ($username, $pw, $dbname, $grants = 'all privileges',
 
   if ($dbname == '*') {
     $real_dbname = "*.*"
-    $privilege_check_query = "select count(*) from information_schema.USER_PRIVILEGES where GRANTEE=\"'${username}'@'${dbhost}'\" and PRIVILEGE_TYPE=\"${grants}\" and IS_GRANTABLE=\"${isgrantable}\""
+    $privilege_check_query = "select count(*) from information_schema.USER_PRIVILEGES where GRANTEE=\"'${username}'@'${host_to_grant}'\" and PRIVILEGE_TYPE=\"${grants}\" and IS_GRANTABLE=\"${isgrantable}\""
   } else {
     $real_dbname = "`${dbname}`.*"
-    $privilege_check_query = "select count(*) from information_schema.SCHEMA_PRIVILEGES where TABLE_SCHEMA=\"'${dbname}'\" GRANTEE=\"'${username}'@'${dbhost}'\" and PRIVILEGE_TYPE=\"${grants}\" and IS_GRANTABLE=\"${isgrantable}"
+    $privilege_check_query = "select count(*) from information_schema.SCHEMA_PRIVILEGES where TABLE_SCHEMA=\"'${dbname}'\" GRANTEE=\"'${username}'@'${host_to_grant}'\" and PRIVILEGE_TYPE=\"${grants}\" and IS_GRANTABLE=\"${isgrantable}"
   }
 
-  exec { "create-grant-${name}-${username}-${dbhost}":
+  exec { "create-grant-${name}-${username}-${host_to_grant}-${dbhost}":
     command => "/usr/bin/mysql -h${dbhost} -u${mariadb::params::admin_user} ${real_admin_pass} -e 'grant ${grants} on ${real_dbname} to `${username}`@`${host_to_grant}` identified by \"${pw}\" ${grantoption}'",
     path    => "/bin:/usr/bin",
     onlyif  => "[ `/usr/bin/mysql -h${dbhost} -u${mariadb::params::admin_user} ${real_admin_pass} -BN -e \"${privilege_check_query}\"` -eq 0 ]",
     require => Package[$mariadb::params::packagename_client]
   }
 }
-
