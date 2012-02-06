89c2e2cde347604c43a7a8ef026676f8b18c6c94
Walter Heck <walter@tribily.com>
2012-02-06 09:06:09 +0200

    Improve grants code to make it possible to grant individual rights.

--- manifests/user.pp
+++ manifests/user.pp
@@ -1,5 +1,30 @@
 # Grant a mysql user permissions to a database
 #
+define mariadb::user::dummyloop($dbname, $grantoption, $isgrantable, $username,
+  $pw, $real_admin_pass, $dbhost, $host_to_grant){
+
+  # un-parse our nastyness to get to the actual grant again
+  $grant_ = regsubst($name, '___', '', 'G')
+  $grant_parts = split($grant_, '__')
+  $grant = $grant_parts[3]
+
+  if ($dbname == '*') {
+    $real_dbname = "*.*"
+    $privilege_check_query = "select count(*) from information_schema.USER_PRIVILEGES where GRANTEE=\\\"'${username}'@'${host_to_grant}'\\\" and PRIVILEGE_TYPE=\\\"${grant}\\\" and IS_GRANTABLE=\\\"${isgrantable}\\\""
+  } else {
+    $real_dbname = "`${dbname}`.*"
+    $privilege_check_query = "select count(*) from information_schema.SCHEMA_PRIVILEGES where TABLE_SCHEMA=\\\"'${dbname}'\\\" GRANTEE=\\\"'${username}'@'${host_to_grant}'\\\" and PRIVILEGE_TYPE=\\\"${grant}\\\" and IS_GRANTABLE=\\\"${isgrantable}\\\""
+  }
+
+  exec { "create-grant-${name}-${username}-${host_to_grant}-${dbhost}":
+    command => "/usr/bin/mysql -h${dbhost} -u${mariadb::params::admin_user} ${real_admin_pass} -e 'grant ${grant} on ${real_dbname} to `${username}`@`${host_to_grant}` identified by \"${pw}\" ${grantoption}'",
+    path    => "/bin:/usr/bin",
+    onlyif  => "[ `/usr/bin/mysql -h${dbhost} -u${mariadb::params::admin_user} ${real_admin_pass} -BN -e \"${privilege_check_query}\"` -eq 0 ]",
+    require => Package[$mariadb::params::packagename_client]
+  }
+
+}
+
 # Usage: mariadb::user { "dbname": "username" }
 define mariadb::user ($username, $pw, $dbname, $grants = 'all privileges',
   $host_to_grant = '%', $dbhost = 'localhost', $withgrants = false) {
@@ -20,18 +45,29 @@ define mariadb::user ($username, $pw, $dbname, $grants = 'all privileges',
     $real_admin_pass = "-p\"${mariadb::params::admin_pass}\""
   }
 
-  if ($dbname == '*') {
-    $real_dbname = "*.*"
-    $privilege_check_query = "select count(*) from information_schema.USER_PRIVILEGES where GRANTEE=\"'${username}'@'${host_to_grant}'\" and PRIVILEGE_TYPE=\"${grants}\" and IS_GRANTABLE=\"${isgrantable}\""
-  } else {
-    $real_dbname = "`${dbname}`.*"
-    $privilege_check_query = "select count(*) from information_schema.SCHEMA_PRIVILEGES where TABLE_SCHEMA=\"'${dbname}'\" GRANTEE=\"'${username}'@'${host_to_grant}'\" and PRIVILEGE_TYPE=\"${grants}\" and IS_GRANTABLE=\"${isgrantable}"
-  }
+  # This is some very nasty puppet stunts to make it possible to run through a string of grants
+  # First surround all individual grants with triple underscores, then prefix each with host,
+  # dbname and username so the $name var for the dummy loop stays unique
 
-  exec { "create-grant-${name}-${username}-${host_to_grant}-${dbhost}":
-    command => "/usr/bin/mysql -h${dbhost} -u${mariadb::params::admin_user} ${real_admin_pass} -e 'grant ${grants} on ${real_dbname} to `${username}`@`${host_to_grant}` identified by \"${pw}\" ${grantoption}'",
-    path    => "/bin:/usr/bin",
-    onlyif  => "[ `/usr/bin/mysql -h${dbhost} -u${mariadb::params::admin_user} ${real_admin_pass} -BN -e \"${privilege_check_query}\"` -eq 0 ]",
-    require => Package[$mariadb::params::packagename_client]
+  #replace all spaces by underscores
+  #$grants = 'INSERT, DELETE'
+  $grants_ = regsubst($grants, '\s*,\s*', "___,___${dbhost}__${dbname}__${username}__", 'G')
+  #$grants_ = 'INSERT__,__DELETE'
+  $grants__ = "___${dbhost}__${dbname}__${username}__${grants_}___"
+  #$grants__ = "__localhost_zabbix_zabbixuser_INSERT__,__DELETE__
+  # split the grants by comma into an array
+  $grants_array = split($grants__,',')
+  #$grants_array =
+
+  # call dummy loop with the array in order to check/create all desired grants
+  mariadb::user::dummyloop{ $grants_array:
+    dbname          => $dbname,
+    grantoption     => $grantoption,
+    isgrantable     => $isgrantable,
+    username        => $username,
+    pw              => $pw,
+    real_admin_pass => $real_admin_pass,
+    dbhost          => $dbhost,
+    host_to_grant   => $host_to_grant
   }
 }
